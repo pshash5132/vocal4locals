7b13cdc3e8de5e53174d4f2c7f00a4fb5e76dac5
pratikcme <101399602+pratikcme@users.noreply.github.com>
2024-10-28 00:27:56 +0530

    Update js.blade.php

--- resources/views/frontend/product/js.blade.php
+++ resources/views/frontend/product/js.blade.php
@@ -1,44 +1,53 @@
 <script>
     var variants = {!!json_encode($product->variants->toArray())!!};
-    document.querySelectorAll('.variantBTN').forEach((button) => {
-        button.addEventListener('click', () => {
-alert(0);
+    // $(document).on("click",".variantBTN",function(){
 
-        document.querySelectorAll('input[name="variantItem"]').forEach((radio) => {
-            radio.checked = false;
-        });
 
-        // Check the radio button inside the clicked div
-        const radio = button.querySelector('input[type="radio"]');
-        if (radio) {
-            radio.checked = true;
-        }
 
-        var variantId = $(this).data('id');
-        var variant = variants.find(function (variant) {
-            return variant.id === variantId;
-        });
+    //     var variantId = $(this).data('id');
+    //     var variant = variants.find(function (variant) {
+    //         return variant.id === variantId;
+    //     });
 
-        $('.selectedVariant').val(variantId);
-        $('.regularPrice').html(currency+variant.price);
-        $('.offerPrice').html(currency+variant.offer_price);
-        $('.discount').html(variant.discount_percent);
-        $('.addToCart').attr('data-variantId',variantId);
-        // $.ajax({
-        //     type:'POST',
-        //     url:"{{route('getVariantDetail')}}",
-        //     data:{variantId:variantId},
-        //     success:function(res){
-        //         if(res.status ==1 ){
-        //             $('.selectedVariant').val(res.data.id);
-        //             $('.regularPrice').html(currency+res.data.price);
-        //             $('.offerPrice').html(currency+res.data.offer_price);
-        //             $('.discount').html(res.data.discountPercent);
-        //             $('.addToCart').attr('data-variantId',res.data.id);
-        //         }
-        //     }
-        // })
+    //     $('.selectedVariant').val(variantId);
+    //     $('.regularPrice').html(currency+variant.price);
+    //     $('.offerPrice').html(currency+variant.offer_price);
+    //     $('.discount').html(variant.discount_percent);
+    //     $('.addToCart').attr('data-variantId',variantId);
+    //     // $.ajax({
+    //     //     type:'POST',
+    //     //     url:"{{route('getVariantDetail')}}",
+    //     //     data:{variantId:variantId},
+    //     //     success:function(res){
+    //     //         if(res.status ==1 ){
+    //     //             $('.selectedVariant').val(res.data.id);
+    //     //             $('.regularPrice').html(currency+res.data.price);
+    //     //             $('.offerPrice').html(currency+res.data.offer_price);
+    //     //             $('.discount').html(res.data.discountPercent);
+    //     //             $('.addToCart').attr('data-variantId',res.data.id);
+    //     //         }
+    //     //     }
+    //     // })
+    // })
+
+    $(document).on("click", ".variantBTN", function() {
+    var variantId = $(this).data('id');
+    var variant = variants.find(function(variant) {
+        return variant.id === variantId;
     });
+
+    // Update variant details as in your original code
+    $('.selectedVariant').val(variantId);
+    $('.regularPrice').html(currency + variant.price);
+    $('.offerPrice').html(currency + variant.offer_price);
+    $('.discount').html(variant.discount_percent);
+    $('.addToCart').attr('data-variantId', variantId);
+
+    // Check the corresponding radio button inside the clicked .variantBTN div
+    $(this).find('input[type="radio"]').prop('checked', true);
+
+    // Uncheck all other radio buttons
+    $('input[name="variantItem"]').not($(this).find('input[type="radio"]')).prop('checked', false);
 });
 
 </script>
