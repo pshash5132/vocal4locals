ae00509b6281aab57934dd26d23bd675cb73a15f
pratikcme <101399602+pratikcme@users.noreply.github.com>
2024-10-27 12:59:20 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(UserSeeder::class);
-        \App\Models\User::factory(50)->create();
+        // \App\Models\User::factory(50)->create();
         $this->call(AdminProfileSeeder::class);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
